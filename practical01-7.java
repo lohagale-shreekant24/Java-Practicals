5f43906bb72fc580c387af6547edd0302385988f
lohagale-shreekant24 <shreekant.lohagale24@pcu.edu.in>
2025-01-17 22:38:28 +0530

    Update practical01-7.java

--- practical01-7.java
+++ practical01-7.java
@@ -1,19 +1,16 @@
 import java.util.Scanner;
 
-public class EvenNumbers {
+public class EvenOddCheck {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter the range (1 to N): ");
-        int n = scanner.nextInt();
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
 
-        System.out.println("Even numbers from 1 to " + n + ":");
-        for (int i = 1; i <= n; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
-            }
+        if (number % 2 == 0) {
+            System.out.println(number + " is an even number.");
+        } else {
+            System.out.println(number + " is an odd number.");
         }
-
-        scanner.close();
     }
 }
